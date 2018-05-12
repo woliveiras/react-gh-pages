ded730634af2a18773adba41efbe652a2ef02d0c
woliveiras <w.oliveira542@gmail.com>
2018-05-12 11:41:44 -0300

    Add app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <h1 className="app__title">Exemplo de React e GitHub Pages</h1>
+            </div>
+        )
+    }
+}
+
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const	UglifyJSPlugin	=	require('uglifyjs-webpack-plugin')
+
+let plugins = [
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.join(__dirname, 'src', 'index.html')
+  }),
+  new ExtractTextPlugin('style.css')
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    }
+  }));
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react'],
+              plugins: ['transform-class-properties']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|ico|png|gif|eot|woff|woff2|ttf|svg)$/i,
+        loader: 'file-loader?name=img/[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './dist'
+  }
+}
